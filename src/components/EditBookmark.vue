e7cf0006334d29b2d20739760625eeee41190856
Agathe Cocco <agathecocco@gmail.com>
2018-05-24 12:10:10 +0100

    finalising project

--- src/components/EditBookmark.vue
+++ src/components/EditBookmark.vue
@@ -4,13 +4,12 @@
       <h1 class="title">Edit bookmark</h1>
     </div>
     <form @submit.prevent="validateForm">
-      <input type="text" placeholder="Site name" v-model.lazy="currentBookmark.name" name="name" v-validate="'required'">
-      <div class="error" v-if="errors.has('name')" :class="{'has-error': errors.has('name')}">{{errors.first('name')}}</div>
+      <input type="text" placeholder="Site name" v-model.lazy="currentBookmark.name" name="name" v-validate="'required'" :class="{'has-error': errors.has('name')}">
+      <div class="error" v-if="errors.has('name')">{{errors.first('name')}}</div>
       <input type="text" placeholder="URL" v-model.lazy="currentBookmark.url" name="url" v-validate="'required|url'" :class="{'has-error': errors.has('url')}">
       <div class="error" v-if="errors.has('url')">{{errors.first('url')}}</div>
       <input type="text" placeholder="Category" v-model="currentBookmark.category" name="category">
       <div class="actions">
-        <div @click="toggleEditForm" class="btn-cancel">Cancel</div>
         <button type="submit" class="btn-submit">Save changes</button>
       </div>
     </form>
@@ -38,7 +37,6 @@
     },
     methods: {
       editBookmark () {
-        console.log('edited')
         this.toggleEditForm()
         // posting the changes to firebase
         axios
